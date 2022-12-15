649969c1647e524e882313654770ca30af0e9f70
masagon972 <tamakoDev@gmail.com>
2022-12-15 22:35:15 +0900

    commit

--- setup.py
+++ setup.py
@@ -3,13 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='suikawari',
     version="0.0.1",
-    packages=find_packages("suikawari"), 
+    packages=find_packages(), 
     description='スイカ割りのゲームです',
-    package_dir={"":"suikawari"},
     url='https://github.com/tamako1025h/suikawari',
     author='tamako',
     author_email='tamakoDev@gmail.com',
-    entry_points = {
-        'console_scripts': ['suikawari = suikawari.main:main']
-    }
 )
